a51ab2c0316b6bceeb7c456c3d8e2e3aeacb9dcb
glorona <glorona@espol.edu.ec>
2022-06-07 22:56:07 -0500

    Agregar FXML con su controlador, clase main y aspecto a ser desarrollado.

---
+++ src/controller/MainController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author usuario
+ */
+public class MainController implements Initializable{
+
+    @FXML
+    private Button color1;
+    @FXML
+    private Button color2;
+    @FXML
+    private Button color3;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+ 
+
+}
